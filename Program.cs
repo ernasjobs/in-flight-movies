fcf37f84fcf150290c609771215cfb72bc64edf8
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-02 20:47:06 +0100

    create 7 movie objects, add movies into movieList

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace in_flight_movies
 {
@@ -6,7 +7,25 @@ namespace in_flight_movies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Movie movie1=new Movie("Miss Bala",104);
+            Movie movie2=new Movie("Happy Death Day ",100);
+            Movie movie3=new Movie("Captain Marvel",109);
+            Movie movie4=new Movie("Wonder Park",95);
+            Movie movie5=new Movie("Hellboy",120);
+            Movie movie6=new Movie("Little",112);
+            Movie movie7=new Movie("Long Shot",104);
+
+            var movieList=new List<Movie>();
+            movieList.Add(movie1);
+            movieList.Add(movie2);
+            movieList.Add(movie3);
+            movieList.Add(movie4);
+            movieList.Add(movie5);
+            movieList.Add(movie6);
+            movieList.Add(movie7);
+
+            
+
         }
     }
 }
