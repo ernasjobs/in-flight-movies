e7dbbff7b9fd5a5894ee23d90160c0b9c596ad5a
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-02 20:45:22 +0100

    Create Movie class, create 7 movie object, add movies into movieList

---
+++ Movie.cs
@@ -0,0 +1,11 @@
+public class Movie
+{
+    public string Title{get;set;}
+    public int Runtime{get;set;}
+    public Movie(string title,int runtime)
+    {
+        Title=title;
+        Runtime=runtime;
+        
+    }
+}
