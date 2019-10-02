d6f63f089e8a397c827ec9b179a3ffafcfcd70b5
Ernas Pikuli <ernasjobs@gmail.com>
2019-10-02 20:26:19 +0100

    Create C# console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace in_flight_movies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
